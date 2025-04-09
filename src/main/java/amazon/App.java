36a021baa7c5c86b55cb2c9438caef73f2e28fd5
Shreyagd <108246217+Shreyagd@users.noreply.github.com>
2025-04-09 17:28:02 +0530

    Update App.java

--- src/main/java/amazon/App.java
+++ src/main/java/amazon/App.java
@@ -6,6 +6,6 @@ package amazon;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-          System.out.println("Hello Shreya!");
+          System.out.println("Hello Sanjana!");
     }
 }
